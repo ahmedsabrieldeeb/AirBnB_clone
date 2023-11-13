c8a5dfd70768f12b4a0fde00cf8c89fe1ed7ef79
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-11-13 02:20:18 +0200

    Edited data types of Place class's attributes
    - Shoud we use integers and floats in some attributes not strings
    - Edited test methods of Place class that could hanlde kwargs not args

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -20,18 +20,30 @@ class TestPlace(unittest.TestCase):
 
     def test_create_place_with_required_arguments(self):
         """
-            Place object can be created with required arguments.
+        Place object can be created with required arguments.
         """
-        place = Place("city_id", "user_id", "name")
+        place = Place(city_id="city_id", user_id="user_id", name="name")
         self.assertEqual(place.city_id, "city_id")
         self.assertEqual(place.user_id, "user_id")
         self.assertEqual(place.name, "name")
 
     def test_create_place_with_all_arguments(self):
         """
-            Place object can be created with all arguments.
+        Place object can be created with all arguments.
         """
-        place = Place("city_id", "user_id", "name", "description", 5, 3, 10, 100, 37.7749, -122.4194, ["amenity1", "amenity2"])
+        place = Place(
+            city_id="city_id",
+            user_id="user_id",
+            name="name",
+            description="description",
+            number_rooms=5,
+            number_bathrooms=3,
+            max_guest=10,
+            price_by_night=100,
+            latitude=37.7749,
+            longitude=-122.4194,
+            amenity_ids=["amenity1", "amenity2"]
+        )
         self.assertEqual(place.city_id, "city_id")
         self.assertEqual(place.user_id, "user_id")
         self.assertEqual(place.name, "name")
@@ -46,43 +58,43 @@ class TestPlace(unittest.TestCase):
 
     def test_save_place(self):
         """
-            Place object can be saved successfully.
+        Place object can be saved successfully.
         """
-        place = Place("city_id", "user_id", "name")
+        place = Place(city_id="city_id", user_id="user_id", name="name")
         place.save()
         self.assertIsNotNone(place.updated_at)
 
     def test_create_place_with_empty_arguments(self):
         """
-            Place object can be created with empty arguments.
+        Place object can be created with empty arguments.
         """
         place = Place()
         self.assertEqual(place.city_id, "")
         self.assertEqual(place.user_id, "")
         self.assertEqual(place.name, "")
         self.assertEqual(place.description, "")
-        self.assertEqual(place.number_rooms, "")
-        self.assertEqual(place.number_bathrooms, "")
-        self.assertEqual(place.max_guest, "")
-        self.assertEqual(place.price_by_night, "")
-        self.assertEqual(place.latitude, "")
-        self.assertEqual(place.longitude, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
         self.assertEqual(place.amenity_ids, [])
 
     def test_create_place_with_invalid_arguments(self):
         """
-            Place object can be created with invalid arguments.
-        """
-        place = Place(123, True, None)
-        self.assertEqual(place.city_id, 123)
-        self.assertEqual(place.user_id, True)
+        Place object can be created with invalid arguments.
+        """     
+        place = Place(city_id='123' , user_id='ABC', name=None)
+        self.assertEqual(place.city_id , '123')
+        self.assertEqual(place.user_id, 'ABC')
         self.assertEqual(place.name, None)
 
     def test_save_place_with_invalid_arguments(self):
         """
-            Place object can be saved with invalid arguments.
+        Place object can be saved with invalid arguments.
         """
-        place = Place("city_id", "user_id", "name")
+        place = Place(city_id="city_id", user_id="user_id", name="name")
         place.save()
         place.city_id = 123
         place.user_id = True
